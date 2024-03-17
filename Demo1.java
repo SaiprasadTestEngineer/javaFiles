1b7e9f5f046999c36a53a6dde189020ed1f6b367
saiprasadTestEngineer <saiprasadpothuganti@gmail>
2024-03-17 11:07:46 +0530

    demo

--- Demo1.java
+++ Demo1.java
@@ -3,7 +3,9 @@ package javaPrograms;
 public class Demo1 {
 
 	public static void main(String[] args) {
-		System.out.println("Good Morning WElcome to our demo program");
+		System.out.println("Good Morning WElcome to our demo program, program starts");
+		System.out.println("this is the first demo program of java");
+		System.out.println("thanks to all");
 
 	}
 
