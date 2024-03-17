6c3ca341a49a497813cf4b5a3fa9b9fe6e4b960e
saiprasadTestEngineer <saiprasadpothuganti@gmail>
2024-03-17 11:28:06 +0530

    Tow demos

--- Demo1.java
+++ Demo1.java
@@ -5,7 +5,7 @@ public class Demo1 {
 	public static void main(String[] args) {
 		System.out.println("Good Morning WElcome to our demo program, program starts");
 		System.out.println("this is the first demo program of java");
-		System.out.println("thanks to all");
+		System.out.println("  thanks to all  ");
 
 	}
 
---
+++ Demo2.java
@@ -0,0 +1,10 @@
+package javaPrograms;
+
+public class Demo2 {
+
+	public static void main(String[] args) {
+		System.out.println("This is Demo2");
+
+	}
+
+}
